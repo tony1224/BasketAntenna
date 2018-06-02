af38bd56b63ba39e9cc0fdaca833f27a15b753cb
Morita Jun <morita@onespal.co.jp>
2018-06-02 17:47:27 +0900

    :recycle: workspaceファイルはgit管理対象外に
    Issue No: なし
    Description: なし

--- BasketAntenna/ViewController.swift
+++ BasketAntenna/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Test Modify
+        // Test Modify2
     }
 
     override func didReceiveMemoryWarning() {

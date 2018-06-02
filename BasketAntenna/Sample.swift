db5c7facad2655f61a898e2f4c0b6759b379fe18
Morita Jun <morita@onespal.co.jp>
2018-06-02 20:09:30 +0900

    :sparkles: テストコードを暫定的に書く
    Issue No: なし
    Description: なし

---
+++ BasketAntenna/Sample.swift
@@ -0,0 +1,17 @@
+//
+//  Sample.swift
+//  BasketAntenna
+//
+//  Created by Morita Jun on 2018/06/02.
+//  Copyright © 2018年 KamuiProject. All rights reserved.
+//
+
+import Foundation
+
+class Sample {
+    
+    func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+}
--- BasketAntennaTests/BasketAntennaTests.swift
+++ BasketAntennaTests/BasketAntennaTests.swift
@@ -11,31 +11,9 @@ import XCTest
 
 class BasketAntennaTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
-    // 画面に表示された文字列があっているか
-    func testLabel() {
-        
+    func test_日本語で初めてテストを書いてみる() {
+        let sample = Sample()
+        XCTAssertEqual(5, sample.add(a: 2, b: 3))
     }
     
 }

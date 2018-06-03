2695e05acc8c6c158493920ec4a1256cdb1b605a
Morita Jun <morita@onespal.co.jp>
2018-06-04 00:44:22 +0900

    :bug: 再テスト
    Issue No: なし
    Description: なし

--- BasketAntenna/ViewController/ViewController.swift
+++ BasketAntenna/ViewController/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Test Fuga
+        // Test Fuga2
     }
 
     override func didReceiveMemoryWarning() {

d8fe82bea097c3d6f5e6c504d0ca1a6154c8c894
Morita Jun <morita@onespal.co.jp>
2018-06-03 22:21:46 +0900

    :recycle: 改めてDevelopを作成
    Issue No: なし
    Description: なし

--- BasketAntenna/ViewController/ViewController.swift
+++ BasketAntenna/ViewController/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Test Danger
+        // Test Develop
     }
 
     override func didReceiveMemoryWarning() {

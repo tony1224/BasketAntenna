e616b87dad3f94c4f8a99e9ae88a5c3feb6e4ea7
kamui_project <starmine2013@gmail.com>
2018-06-17 16:52:06 +0900

    Refactoring ViewController/Storyboard

--- BasketAntenna/ViewController/ViewController.swift
+++ BasketAntenna/ViewController/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Test Hoge
+        print("Refactoring Master")
     }
 
     override func didReceiveMemoryWarning() {
